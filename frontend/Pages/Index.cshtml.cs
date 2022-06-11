12461efd1dbd52af24a019ff5c5389c5a39ccd84
Serge van den Oever <svdoever@gmail.com>
2022-06-11 23:51:46 +0200

    first commit

---
+++ frontend/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace frontend.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+    public WeatherForecast[] Forecasts { get; set; }
+    public async Task OnGet([FromServices] WeatherClient client)
+    {
+        Forecasts = await client.GetWeatherAsync();
+    }
+}
